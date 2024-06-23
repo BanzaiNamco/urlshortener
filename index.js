2a7e6610635206e15cc8ff48299412b6525c29e6
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2024-06-23 16:40:32 +0800

    test

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+var urls = [];
+
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -13,6 +15,23 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/shorturl', function(req, res) {
+  let url = req.body.url;
+  let index = urls.length;
+  urls.push(url);
+  res.send({ original_url: url, short_url: index });
+});
+
+app.get('/api/shorturl/:index', function(req, res) {
+  let index = req.params.index;
+  if (index >= urls.length) {
+    res.send({ error: 'Invalid short url' });
+    return;
+  }
+  let url = urls[index];
+  res.redirect(url);
+});;
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
