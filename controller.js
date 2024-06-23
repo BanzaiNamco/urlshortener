72c0457253f97e3c695c0bdf128094058f242e3e
Orrin Uy <orrin_landon_uy@dlsu.edu.ph>
2024-06-23 09:38:52 +0000

    a

--- controller.js
+++ controller.js
@@ -11,9 +11,8 @@ const controller = {
     getURL: (req, res) => {
         let index = req.params.index;
         // check if url is valid format
-        const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(\/\S*)?$/;
+        const urlPattern = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(\/\S*)?$/;
         if (!urlPattern.test(urls[index])) {
-            console.log('invalid url');
             res.send({ error: 'invalid url' });
         }
 
