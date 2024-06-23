8b02e4a9e2d146b907def961c23ebf49b5ad18e4
Orrin Uy <orrin_landon_uy@dlsu.edu.ph>
2024-06-23 09:01:09 +0000

    .

--- controller.js
+++ controller.js
@@ -10,13 +10,19 @@ const controller = {
     },
     getURL: (req, res) => {
         let index = req.params.index;
+        // check if url is valid format
+        const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(\/\S*)?$/;
+        if (!urlPattern.test(urls[index])) {
+            console.log('invalid url');
+            res.send({ error: 'invalid url' });
+        }
+
         if (index >= urls.length) {
-            res.send({ error: 'Invalid short url' });
-            return;
+            res.send({ error: 'invalid url' });
         }
         let url = urls[index];
         res.redirect(url);
     }
 }
 
-export default controller;
+module.exports = controller;
